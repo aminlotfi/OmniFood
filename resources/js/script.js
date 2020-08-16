fcae8e90a817504b98d785e8669907aeaac71716
aminlotfi <a.lotfi1201@gmail.com>
2020-08-16 21:32:25 +0430

    scrolling to elements

--- resources/js/script.js
+++ resources/js/script.js
@@ -1,4 +1,6 @@
 $(document).ready(function () {
+
+    /** for sticky navigation **/
     $('.js--section-features').waypoint(function (direction) {
         if (direction === "down") {
             $('nav').addClass('sticky');
@@ -8,4 +10,30 @@ $(document).ready(function () {
     }, {
         offset: '60px;'
     });
+
+    /** scroll on buttons **/
+    $('.js--scroll-to-plans').click(function () {
+        $('html, body').animate({scrollTop: $('.js--section-plans').offset().top}, 1000);
+    });
+
+    $('.js--scroll-to-start').click(function () {
+        $('html, body').animate({scrollTop: $('.js--section-features').offset().top}, 1000);
+    });
+
+    /** Navigation scroll **/
+    $(function() {
+        $('a[href*=#]:not([href=#])').click(function() {
+            if (location.pathname.replace(/^\//,'') === this.pathname.replace(/^\//,'') && location.hostname === this.hostname) {
+                var target = $(this.hash);
+                target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
+                if (target.length) {
+                    $('html,body').animate({
+                        scrollTop: target.offset().top
+                    }, 1000);
+                    return false;
+                }
+            }
+        });
+    });
+
 });
